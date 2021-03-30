2f5ce29fc1beacb8da1c1174237047bee0bec00e
Mario.Koco <mario.koco92@gmail.com>
2021-03-30 15:50:45 +0200

    impostato script di partenza con click

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Creo una funzione che avvi il mio script tramite pulsante "calculate"
+// 
+// Salvo in una variabile l'elemento "button-calculate" presente nel HTML tramite il suo id.
+var calculateButton = document.getElementById("calculate");
+calculateButton.addEventListener("click", function() {
+    // Imposto una serie di azioni che verranno avviate dopo il "click" del button.
+    
+});
